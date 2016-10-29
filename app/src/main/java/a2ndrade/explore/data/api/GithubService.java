2a5f9d38ad1d75ce08bebd3755896a25f43d4f04
amilcar-andrade <amilcar.andrade.g@gmail.com>
2016-10-29 16:38:49 -0400

    Adds basic GitHubService service

---
+++ app/src/main/java/a2ndrade/explore/data/api/GithubService.java
@@ -0,0 +1,27 @@
+package a2ndrade.explore.data.api;
+
+import java.util.List;
+
+import a2ndrade.explore.data.model.IntegrationCategory;
+import a2ndrade.explore.data.model.User;
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+public interface GitHubService {
+
+    final String BASE_END_POINT = "https://api.github.com";
+    final String INTEGRATION_END_POINT = "https://gist.githubusercontent.com";
+
+    @GET("/users/{user}")
+    void getUser(@Path("user") String user, Callback<User> callback);
+
+    @GET("/users/{user}/followers")
+    List<User> getFollowers(@Path("user") String user);
+
+    @GET("/users/{user}/following")
+    List<User> getFollowing(@Path("user") String user);
+
+    @GET("/amilcar-andrade/4a9590f35b1636069ac74b893924db1a/raw/b380031b3bc916de05ad431451da1ea6457ffa03")
+    List<IntegrationCategory> getIntegrations();
+}
